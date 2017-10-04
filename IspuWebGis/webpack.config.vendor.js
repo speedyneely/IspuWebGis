15645f919f90a02f4f91812d1450b2e6e29ea5c4
Andrei Shilkin <andrei.shilkin@akvelon.com>
2017-10-04 21:48:26 +0300

    @types для arcgis-js-api были установлены. arcgis-esri-loader был установлен.

--- IspuWebGis/webpack.config.vendor.js
+++ IspuWebGis/webpack.config.vendor.js
@@ -13,6 +13,7 @@ const treeShakableModules = [
     '@angular/platform-browser-dynamic',
     '@angular/router',
     'zone.js',
+    'angular-esri-loader'
 ];
 const nonTreeShakableModules = [
     'bootstrap',
