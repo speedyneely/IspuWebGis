a503086ed7d2609072769b2412d46155e0bfde19
SSharyk <ssharyk@gmail.com>
2017-10-07 21:18:23 +0400

    Адаптация верстки для различных экранов
    * Карта теперь занимает все предоставляемое ей свободное место
    * Исправлен контейнер маршрута для непрозрачного накладывания на карту при необходимости

--- IspuWebGis/ClientApp/app/components/app/app.component.ts
+++ IspuWebGis/ClientApp/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app',
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     points: Array<String>
 
+    mapInnerHeight: string;
+    pointsContainer: HTMLElement | null;
+
     center: __esri.PointProperties = {
         longitude: 40.971,
         latitude: 56.997,
     };
+
 
     centerChange(point: __esri.Point) {
         console.log('Center of map was changed: ' + '[longitude: ' + point.longitude + ', latitude: ' + point.latitude + ']');
@@ -24,6 +28,30 @@ export class AppComponent implements OnInit {
         this.points = [];
         for (var i = 0; i < 15; i++) {
             this.points.push(`Point # ${i}`);
+        }
+
+        let footer = document.getElementById("points-container");
+        if (footer != null) this.pointsContainer = <HTMLElement | null>(footer.firstChild);
+
+        window.onresize = this.onWindowResize;
+        this.onWindowResize();
+    }
+
+    private onWindowResize() {
+        if (this.pointsContainer != null) {
+            let footerHeight = +this.pointsContainer.clientHeight;
+            this.mapInnerHeight = (window.innerHeight - footerHeight).toString();
+        }
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+        if (this.pointsContainer != null) {
+            let footerHeight = +this.pointsContainer.clientHeight;
+            this.mapInnerHeight = (window.innerHeight - footerHeight).toString();
+            if (+this.mapInnerHeight > window.innerHeight) {
+                this.mapInnerHeight = window.innerHeight.toString();
+            }
         }
     }
 }
